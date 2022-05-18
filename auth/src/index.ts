69b330629c2c129caa3403eca8b126b4367bd505
Nikita Kuduhovi <dumb_animal@icloud.com>
2022-05-18 12:08:52 +0300

    Add error handler and base routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,28 @@
 import express from "express";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
+import { errorHandler } from "./middlewares/error-handler";
+
 const PORT = 3000;
 const app = express();
 
+// MIDDLEWARE
 app.use(express.json());
 
-app.get("/api/users/current", (req, res) => {
-  res.send("Hi there!");
-});
+// ROUTERS
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
+// ERROR HANDLER
+app.use(errorHandler);
+
+// EXPORTS
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
-})
+});
