e33b96db085afb3108d50465b895ef0a7fd0f110
Nikita Kuduhovi <dumb_animal@icloud.com>
2022-05-23 21:24:24 +0300

    Add new middlewares

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,18 +1,14 @@
 import express from "express";
-import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
+import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  console.log(req.session?.jwt);
-  if (!req.session?.jwt) return res.status(200).json({ currentUser: null });
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    return res.status(200).json({ currentUser: payload })
-  } catch (err) {
-    return res.status(200).json({ currentUser: null })
-  }
-});
+router.get("/api/users/currentuser",
+  currentUser,
+  requireAuth,
+  (req, res) => {
+    res.status(200).json({ currentUser: req.currentUser || null });
+  });
 
 export { router as currentUserRouter };
