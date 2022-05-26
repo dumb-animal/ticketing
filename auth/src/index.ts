f2027fb1570d9726f09bfd444ddebb888aad311d
Nikita Kuduhovi <dumb_animal@icloud.com>
2022-05-26 07:07:07 +0300

    Add tests

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,42 +1,15 @@
-import express from "express";
-import "express-async-errors";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
+import app from "./app";
 
 const PORT = 3000;
-const app = express();
+const MONGO_URI = "mongodb://auth-mongo-service:27017/auth";
 
-// MIDDLEWARE
-app.set('trust proxy', true);
-
-app.use(express.json());
-app.use(cookieSession({ signed: false, secure: true }))
-
-// ROUTERS
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-// ERROR HANDLER
-app.all("*", (req, res, next) => next(new NotFoundError()));
-app.use(errorHandler);
-
-// SERVER
 const start = async () => {
   // CHECK ENVIRONMENTS
   if (!process.env.JWT_KEY) throw new Error("JWT_KEY is not defined");
 
   // DB CONNECTION
-  await mongoose.connect("mongodb://auth-mongo-service:27017/auth")
+  await mongoose.connect(MONGO_URI)
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("Error connecting to database: ", err));
 
---
+++ auth/src/test/setup.ts
@@ -0,0 +1,44 @@
+// import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from "supertest";
+import app from "../app";
+
+declare global {
+  function signin(): Promise<string[]>
+};
+
+beforeAll(async () => {
+  process.env.JWT_KEY = "hello";
+
+  // var mongod = await MongoMemoryServer.create();
+  // globalThis.__MONGOD__ = mongod;
+  // const mongoUri = mongod.getUri();
+  // await mongoose.connect(mongoUri)
+
+  await mongoose.connect("mongodb://localhost:27017/test")
+});
+
+beforeEach(async () => {
+  await mongoose.connection.db.dropDatabase();
+  // const collections = await mongoose.connection.db.collections();
+  // collections.forEach(async (c) => await c.deleteMany({}));
+});
+
+afterAll(async () => {
+  // await globalThis.__MONGOD__.stop();
+  await mongoose.connection.db.dropDatabase();
+  await mongoose.connection.close();
+});
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+  return cookie;
+}
