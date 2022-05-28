8f46c85d8eb79def2eb70ae44cb83486236c229e
Nikita Kuduhovi <dumb_animal@icloud.com>
2022-05-28 09:03:25 +0300

    Update k8s config and add env vars

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,21 +1,20 @@
 import mongoose from "mongoose";
 import app from "./app";
 
-const PORT = 3000;
-const MONGO_URI = "mongodb://auth-mongo-service:27017/auth";
-
 const start = async () => {
   // CHECK ENVIRONMENTS
+  if (!process.env.PORT) throw new Error("PORT is not defined");
   if (!process.env.JWT_KEY) throw new Error("JWT_KEY is not defined");
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI is not defined");
 
   // DB CONNECTION
-  await mongoose.connect(MONGO_URI)
+  await mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("Error connecting to database: ", err));
 
   // LISTENER
-  app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
+  app.listen(process.env.PORT, () => {
+    console.log(`Listening on port: ${process.env.PORT}`);
   });
 }
 
