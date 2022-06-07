3c98a48bb457ece4523dfb323ca9d601f8d7fa04
Nikita Kuduhovi <dumb_animal@icloud.com>
2022-06-07 09:20:37 +0300

    Delete nats-test

--- nats-test/src/events/ticket-created-publisher.ts
+++
@@ -1,7 +0,0 @@
-import { Publisher } from "./base-publisher";
-import { TicketCreatedEvent } from "./ticket-created-event";
-import { Subjects } from "./subjects";
-
-export default class TickeCreatedPublisher extends Publisher<TicketCreatedEvent> {
-  subject: Subjects.TicketCreated = Subjects.TicketCreated;
-};
